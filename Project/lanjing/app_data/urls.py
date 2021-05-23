1b044184e56f4b9a7495fb498e3527adb9e13fe2
akala515 <73008460+akala515@users.noreply.github.com>
2021-05-23 13:47:12 +0800

    Delete Project/lanjing directory

--- Project/lanjing/app_data/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app_data import views
-urlpatterns = [
-    path('', views.index),
-    path('add/', views.add),
-    path('update/', views.update),
-    path('delete/', views.delete),
-    path('search/', views.search),
-]
